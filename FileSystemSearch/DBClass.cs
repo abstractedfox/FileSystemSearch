f90f771794bd49b10511c5753390c8899d6a39ab
Chris <chriswhotakesphotos@gmail.com>
2022-10-23 19:19:23 -0400

    Some progress made on db model

--- FileSystemSearch/DBClass.cs
+++ FileSystemSearch/DBClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,20 +27,23 @@ namespace FileSystemSearch
 
     public class DataItem
     {
-        int Id { get; set; }
-        string CaseInsensitiveFilename { get; set; }
-        string FullPath { get; set; }
+        [Key]
+        public long Id { get; set; }
+
+        public string CaseInsensitiveFilename { get; set; }
+        public string FullPath { get; set; }
         //Dictionary<int, int> PositionInPatternList = new Dictionary<int, int>();
         //dictionary pattern is: PatternListID:PrimaryKey of this item in that list
 
-        List<int> PatternListsContainingThis = new List<int>();
+        public List<int> PatternListsContainingThis = new List<int>();
     }
 
     public class PatternList
     {
-        int Id { get; set; }
-        int Size { get; set; }
-        string pattern { get; set; }
-        List<int> ForeignKeyInMainList = new List<int>();
+        public long Id { get; set; }
+
+        public int Size { get; set; }
+        public string pattern { get; set; }
+        public List<int> ForeignKeyInMainList = new List<int>();
     }
 }
---
+++ FileSystemSearch/Migrations/20221020175821_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileSystemSearch.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DataItems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CaseInsensitiveFilename = table.Column<string>(type: "TEXT", nullable: false),
+                    FullPath = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatternLists",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Size = table.Column<int>(type: "INTEGER", nullable: false),
+                    pattern = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatternLists", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataItems");
+
+            migrationBuilder.DropTable(
+                name: "PatternLists");
+        }
+    }
+}
